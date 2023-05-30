c1a86fdef37d91f8584436e0d08ec69262b34670
Andrey <andreypomortsev@gmail.com>
2023-05-30 18:34:06 +0500

    Change import line from source

--- tests/test_change_image.py
+++ tests/test_change_image.py
@@ -1,7 +1,7 @@
 import unittest
 import os.path
 from PIL import Image
-from source.change_image import changer, DESTINATION
+from source import change_image
 
 
 class TestBulkImageProcess(unittest.TestCase):
@@ -18,27 +18,27 @@ class TestBulkImageProcess(unittest.TestCase):
 
     def tearDown(self):
         for img in self.test_images:
-            os.remove(os.path.join(DESTINATION, img))
+            os.remove(os.path.join(change_image.DESTINATION, img))
         os.rmdir(self.test_dir)
 
     def test_changer_resizes_and_saves_image(self):
         img_path = os.path.join(self.test_dir, "apple.jpg")
-        changer(img_path)
-        expected_path = os.path.join(DESTINATION, "apple.jpg")
+        change_image.changer(img_path)
+        expected_path = os.path.join(change_image.DESTINATION, "apple.jpg")
         self.assertTrue(os.path.exists(expected_path))
         with Image.open(expected_path) as img:
             self.assertEqual(img.size, (600, 400))
 
     def test_changer_raises_error_if_image_file_not_found(self):
         with self.assertRaises(Exception):
-            changer("nonexistent.jpg")
+            change_image.changer("nonexistent.jpg")
 
     def test_changer_raises_error_if_file_is_not_image(self):
         txt_path = os.path.join(self.test_dir, "file.txt")
         with open(txt_path, "w") as txt_file:
             txt_file.write("this is a text file")
         with self.assertRaises(Exception):
-            changer(txt_path)
+            change_image.changer(txt_path)
 
 
 if __name__ == "__main__":

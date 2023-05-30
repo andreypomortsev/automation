1b1a910b19c569560d7597ca3950ce33ee1f5321
Andrey <andreypomortsev@gmail.com>
2023-05-30 13:01:10 +0500

    Change of the test: added mock_open to the import,

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,7 +1,5 @@
 import unittest
-from unittest.mock import patch, Mock
-import io
-import sys
+from unittest.mock import patch, mock_open
 from source.run import read_txt
 
 class TestUpload(unittest.TestCase):
@@ -9,9 +7,9 @@ class TestUpload(unittest.TestCase):
         self.txt_file = "example.txt"
 
     def test_read_txt(self):
-        with patch('builtins.open', create=True) as mock_open:
-            mock_text = "Apple\n200 lbs\nA juicy red apple"
-            mock_open.return_value.__enter__.return_value.read.return_value = mock_text
+        mock_text = "Apple\n200 lbs\nA juicy red apple"
+        with patch('builtins.open', mock_open(read_data=mock_text)) as mock_file:
+            mock_file.return_value.__enter__.return_value.read.return_value = mock_text
             expected_output = "name: Apple\nweight: 200 lbs"
             output = read_txt(self.txt_file, False)
             self.assertEqual(output, expected_output)

da86e4e11561fa2ee2cd2a3e61be94e07b01090f
Andrey <andreypomortsev@gmail.com>
2023-05-30 16:10:32 +0500

    Add docstrings to the class and each its method

--- tests/test_report_email.py
+++ tests/test_report_email.py
@@ -1,20 +1,50 @@
 import unittest
-from unittest.mock import patch, MagicMock
 import os
-from source import reports
-from source import report_email
+import shutil
+from source import emails, reports, run
+from source import report_email, full_path
+
 
 class TestMain(unittest.TestCase):
-    def test_main(self):
-        # Set up mock environment variables
-        os.environ["USER"] = "testuser"
-        mock_glob = MagicMock(return_value=["file1.txt", "file2.txt"])
-        with patch("source.report_email.glob.glob", mock_glob):
-            # Call main() function
-            report_email.main()
-            # Assert that reports.generate_report and emails.send were called with the expected arguments
-            reports.generate_report.assert_called_once_with("/tmp/processed.pdf", "Sales summary for last month", "summary1<br/>summary2")
-            emails.send.assert_called_once()
+    """Test source/report_emails.py
+    """
+    def setUp(self) -> None:
+        """Create folders from `full_path`, and txt files
+        to check the modulde. 
+        """
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+        self.files = ("1.txt", "2.txt", "3.txt")
+        self.lines = []
+        for number, file in enumerate(self.files):
+            with open(file, "w", encoding="UTF-8") as mock_file:
+                line = f"{number}Apple\n{number}00 lbs\nSuch a tasty {number} Apple\n"
+                self.lines.append(line)
+                mock_file.write(line)
 
-if __name__ == '__main__':
+    def tearDown(self) -> None:
+        """Tear down all the folders and files from full_path
+        by deleting the folder, which is one folder up from the `full_path` 
+        """
+        os.chdir(full_path)
+        os.path.dirname(full_path)
+        shutil.rmtree(os.getcwd())
+
+    def test_main(self) -> None:
+        """Check if the report_emails.py call the needed functions with
+        the right params, which a variable `summary` hold.
+        """
+        report_email.main()
+        # Assert that reports.generate_report and emails.send
+        # were called with the expected arguments
+        summary = [run.read_txt(line) for line in self.lines]
+        reports.generate_report.assert_called_once_with(
+            "/tmp/processed.pdf",
+            "Upload Completed - Online Fruit Store",
+            "<br/>".join(summary),
+        )
+        emails.send.assert_called_once()
+
+
+if __name__ == "__main__":
     unittest.main()

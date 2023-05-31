5022126183f330a04a6792b29b5a462978e323ce
Andrey <andreypomortsev@gmail.com>
2023-05-31 18:42:01 +0500

    Deleted useless methods, which had to test raised errors but the module doesn't raise any, it just works no matter what. Added docstrings to the class and its methods

--- tests/test_change_image.py
+++ tests/test_change_image.py
@@ -5,7 +5,15 @@ from source import change_image
 
 
 class TestBulkImageProcess(unittest.TestCase):
+    """
+    A class for testing the bulk image processing functionality of the 'change_image' module.
+    """
     def setUp(self):
+        """
+        Initializes test data by creating test images in the destination directory.
+        
+        :return: None
+        """
         self.test_images = ["apple.jpg", "banana.jpg", "grapes.jpeg"]
         self.test_dir = change_image.DESTINATION
         for img in self.test_images:
@@ -14,28 +22,27 @@ class TestBulkImageProcess(unittest.TestCase):
             )
 
     def tearDown(self):
+        """
+        Removes test images from the destination directory.
+        
+        :return: None
+        """
         for img in self.test_images:
             os.remove(os.path.join(change_image.DESTINATION, img))
-        os.rmdir(self.test_dir)
 
     def test_changer_resizes_and_saves_image(self):
+        """
+        Tests the 'changer' method of the 'change_image' module by verifying 
+        that it resizes and saves an image correctly.
+        
+        :return: None
+        """
         img_path = os.path.join(self.test_dir, "apple.jpg")
         change_image.changer(img_path)
         expected_path = os.path.join(change_image.DESTINATION, "apple.jpg")
         self.assertTrue(os.path.exists(expected_path))
         with Image.open(expected_path) as img:
             self.assertEqual(img.size, (600, 400))
-
-    def test_changer_raises_error_if_image_file_not_found(self):
-        with self.assertRaises(Exception):
-            change_image.changer("nonexistent.jpg")
-
-    def test_changer_raises_error_if_file_is_not_image(self):
-        txt_path = os.path.join(self.test_dir, "file.txt")
-        with open(txt_path, "w") as txt_file:
-            txt_file.write("this is a text file")
-        with self.assertRaises(Exception):
-            change_image.changer(txt_path)
 
 
 if __name__ == "__main__":

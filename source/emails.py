05d891d4d48cbb6267dcae68e05ce93bb7b3aab8
Andrey <andreypomortsev@gmail.com>
2023-06-01 10:20:31 +0500

    Add a possibility to create an email.message w/o attachement, some rafactoring

--- source/emails.py
+++ source/emails.py
@@ -4,7 +4,8 @@
 This module provides functions to generate an email with an attachment and send it via SMTP servers.
 
 Functions:
-- generate(sender, recipient, subject, body, attachment_path): Creates an email with an attachment.
+- generate(sender: str, recipient: str, subject: str, body: str, attachment_path: str): 
+Creates an email with an attachment.
 - send(message): Sends the message to the configured SMTP server.
 
 Author: Andrey Pomortsev
@@ -17,26 +18,31 @@ import smtplib
 
 
 def generate(
-    sender: str, recipient: str, subject: str, body: str, attachment_path: str
+    sender: str,
+    recipient: str,
+    subject: str,
+    body: str,
+    attachment_path: str | None = None,
 ) -> email.message.EmailMessage:
-    """Creates an email with an attachement."""
+    """Creates an email with or w/o an attachement."""
     message = email.message.EmailMessage()
     message["From"] = sender
     message["To"] = recipient
     message["Subject"] = subject
     message.set_content(body)
 
-    # Process the attachment and add it to the email
-    attachment_filename = os.path.basename(attachment_path)
-    mime_type, _ = mimetypes.guess_type(attachment_path)
-    mime_type, mime_subtype = mime_type.split("/", 1)
-    with open(attachment_path, "rb") as path:
-        message.add_attachment(
-            path.read(),
-            maintype=mime_type,
-            subtype=mime_subtype,
-            filename=attachment_filename,
-        )
+    if attachment_path:
+        # Process the attachment and add it to the email
+        attachment_filename = os.path.basename(attachment_path)
+        mime_type, _ = mimetypes.guess_type(attachment_path)
+        mime_type, mime_subtype = mime_type.split("/", 1)
+        with open(attachment_path, "rb") as path:
+            message.add_attachment(
+                path.read(),
+                maintype=mime_type,
+                subtype=mime_subtype,
+                filename=attachment_filename,
+            )
     return message
 
 

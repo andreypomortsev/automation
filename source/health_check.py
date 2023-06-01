13566f3f60dff2729f234f697838551d0783e5bf
Andrey <andreypomortsev@gmail.com>
2023-06-01 12:13:47 +0500

    Code refactoring

--- source/health_check.py
+++ source/health_check.py
@@ -1,16 +1,17 @@
 import os
-import psutil
 import shutil
 import socket
+import psutil
 from source import emails
 
 BODY = "Please check your system and resolve the issue as soon as possible."
 SENDER = "automation@example.com"
 RECIPIENT = f"{os.environ.get('USER')}@example.com"
 
+
 def check_cpu() -> str | None:
     """
-    Check if the CPU usage is above 80%. If so, generate an email message with 
+    Check if the CPU usage is above 80%. If so, generate an email message with
     the subject "Error - CPU usage is over 80%" and the body text in the variable BODY,
     and send the message using the 'emails' module.
 
@@ -21,23 +22,25 @@ def check_cpu() -> str | None:
         return subject
     return None
 
+
 def check_ram() -> str | None:
     """
-    Check if available memory is less than 500 MB. If so, generate an email message with 
-    the subject "Error - Available memory is less than 500MB" and the body text in the variable BODY,
+    Check if available memory is less than 500 MB. If so, generate an email message with
+    the subject "Error - Available memory is less than 500MB" and the body text in the variable BODY
     and send the message using the 'emails' module.
 
     :return: None
     """
-    available_memory = psutil.virtual_memory().available/(1024*1024)
+    available_memory = psutil.virtual_memory().available / (1024 * 1024)
     if available_memory < 500:
         subject = "Error - Available memory is less than 500MB"
         return subject
     return None
-        
+
+
 def check_disk() -> str | None:
     """
-    Check if free disk space is less than 20%. If so, generate an email message 
+    Check if free disk space is less than 20%. If so, generate an email message
     with the subject "Error - Available disk space is less than 20%" and the body text
     in the variable BODY, and send the message using the 'emails' module.
 
@@ -48,23 +51,24 @@ def check_disk() -> str | None:
         subject = "Error - Available disk space is less than 20%"
         return subject
     return None
-        
+
+
 def check_network() -> str | None:
     """
-    Check if the local host can access the internet. If not, generate an email message 
+    Check if the local host can access the internet. If not, generate an email message
     with the subject "Error - localhost cannot be resolved to 127.0.0.1" and the body text
     in the variable BODY, and send the message using the 'emails' module.
     """
     try:
-        socket.gethostbyname('127.0.0.1')
+        socket.gethostbyname("127.0.0.1")
         return None
     except socket.error:
         subject = "Error - localhost cannot be resolved to 127.0.0.1"
         return subject
 
+
 def system_check() -> None:
-    """ Test the system resources and send an email if there is something wrong
-    """
+    """Test the system resources and send an email if there is something wrong"""
     for func in (check_cpu, check_disk, check_network, check_ram):
         subject = func()
         if not subject:

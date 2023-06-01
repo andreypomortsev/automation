1702368adb819d0752e3bdf8ac35727dc4ee4fa1
Andrey <andreypomortsev@gmail.com>
2023-06-01 10:24:43 +0500

    Remove self.message from setUp to the test functions, due to change in the emails.py add a test for generate message funct w/o attachement

--- tests/test_emails.py
+++ tests/test_emails.py
@@ -14,32 +14,51 @@ class TestEmails(unittest.TestCase):
         # Create the attachment file
         with open(self.attachment_path, "w") as f:
             f.write("test text")
-        self.message = emails.generate(self.sender, self.recipient, self.subject, self.body, self.attachment_path)
-    
+
     def tearDown(self) -> None:
         os.remove(self.attachment_path)
-    
-    def test_email_generate(self):
-        """Test generate() in emails.py
-        """
+
+    def test_email_generate_with_attachement(self):
+        """Test generate() with attachement"""
         # Assert that the generated message has the correct attributes
-        self.assertEqual(self.message["From"], self.sender)
-        self.assertEqual(self.message["To"], self.recipient)
-        self.assertEqual(self.message["Subject"], self.subject)
-        self.assertEqual(self.message.get_body(('plain',)).get_content().strip('\n'), self.body)
+        message = emails.generate(
+            self.sender, self.recipient, self.subject, self.body, self.attachment_path
+        )
+        self.assertEqual(message["From"], self.sender)
+        self.assertEqual(message["To"], self.recipient)
+        self.assertEqual(message["Subject"], self.subject)
+        self.assertEqual(
+            message.get_body(("plain",)).get_content().strip("\n"), self.body
+        )
 
         attachment_filename = os.path.basename(self.attachment_path)
-        attachment = self.message.get_payload()[1]
+        attachment = message.get_payload()[1]
         self.assertEqual(attachment.get_filename(), attachment_filename)
         self.assertEqual(attachment.get_content_type(), "text/plain")
-    
-    @mock.patch('smtplib.SMTP')
+
+    def test_email_generate_wo_attachement(self):
+        """Test generate() w/o attachement"""
+        # Assert that the generated message has the correct attributes
+        message = emails.generate(
+            self.sender, self.recipient, self.subject, self.body, self.attachment_path
+        )
+        self.assertEqual(message["From"], self.sender)
+        self.assertEqual(message["To"], self.recipient)
+        self.assertEqual(message["Subject"], self.subject)
+        self.assertEqual(
+            message.get_body(("plain",)).get_content().strip("\n"), self.body
+        )
+
+    @mock.patch("smtplib.SMTP")
     def test_emails_send_with_server(self, mock_smtp):
-        emails.send(self.message, mock_smtp)
+        message = emails.generate(
+            self.sender, self.recipient, self.subject, self.body, self.attachment_path
+        )
+        emails.send(message, mock_smtp)
         mock_smtp.assert_not_called()
-        mock_smtp.send_message.assert_called_once_with(self.message)
+        mock_smtp.send_message.assert_called_once_with(message)
         mock_smtp.quit.assert_called_once()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
